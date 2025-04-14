a4792046991463d3021cfd947c458ba6eb99fac8
Ranjeet Hambire <ranjeethambire96@gmail.com>
2025-04-14 21:44:36 +0530

    Initial project structure with backend and frontend

---
+++ backend/meter-data-management-system/src/main/java/com/seroter/meter_data_management_system/MeterDataManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.seroter.meter_data_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterDataManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterDataManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/meter-data-management-system/src/test/java/com/seroter/meter_data_management_system/MeterDataManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seroter.meter_data_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeterDataManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
